0f743dbf55b7b95d86c3878cd049cac9e2376ed2
okamu <shogo@okamu.ro>
2018-10-28 12:28:49 +0900

    Initial Commit

---
+++ KamatteiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KamatteiOS
+//
+//  Created by shogo okamuro on 2018/10/28.
+//  Copyright © 2018 shogo okamuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
